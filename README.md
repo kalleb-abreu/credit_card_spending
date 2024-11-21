484c2dc235d87732e725f4a0e92f976a7ab66fac
kalleb.abreu <kalleb.abreu@outlook.com>
2024-11-20 23:23:58 -0300

    feat: added the raw dataset and created an R script for data cleaning

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # credit_card_spending
+
+conda create --name credit_card_spending
+
+conda search --override-channels --channel conda-forge r-base
+
+conda install conda-forge::r-base
+
+conda install conda-forge::r-quarto
+
---
+++ data_cleaning.R
@@ -0,0 +1,47 @@
+if (!require("dplyr")) {
+  install.packages("dplyr")
+  library(dplyr)
+} else {
+  library(dplyr)
+}
+
+df <- read.csv("data/dataset.csv", sep = ";", dec = ",")
+
+unwanted_cols <- c("limite_total", "limite_disp", "grupo_estabelecimento", 
+                  "cidade_estabelecimento", "pais_estabelecimento")
+df <- df[!names(df) %in% unwanted_cols]
+
+unique_ids <- unique(df$id)
+id_mapping <- data.frame(
+  new_id = sprintf("%04d", 1:length(unique_ids)),
+  original_id = unique_ids
+)
+
+df$id <- id_mapping$new_id[match(df$id, id_mapping$original_id)]
+df$safra_abertura <- as.character(df$safra_abertura)
+df$cidade <- as.character(df$cidade)
+df$estado <- as.factor(df$estado)
+df$idade <- as.integer(df$idade)
+df$sexo <- as.factor(df$sexo)
+df$data <- as.Date(df$data, format = "%d.%m.%Y")
+df$valor <- as.numeric(gsub(",", ".", df$valor))
+
+df$mes <- format(df$data, "%m")
+df$ano <- format(df$data, "%Y")
+df$data <- NULL
+
+df_grouped <- df %>%
+  group_by(id, mes, ano) %>%
+  summarise(
+    safra_abertura = names(sort(table(safra_abertura), decreasing = TRUE))[1],
+    cidade = names(sort(table(cidade), decreasing = TRUE))[1],
+    estado = names(sort(table(estado), decreasing = TRUE))[1],
+    idade = as.integer(names(sort(table(idade), decreasing = TRUE))[1]),
+    sexo = names(sort(table(sexo), decreasing = TRUE))[1],
+    total_valor = sum(valor, na.rm = TRUE),
+    n_transactions = sum(!is.na(valor)),
+    .groups = 'drop'
+  ) %>%
+  arrange(id, ano, mes)
+
+write.csv(df_grouped, "data/cleaned_dataset.csv", row.names = FALSE)
